9ca5d7093fa17b2ba9ef83f3855d7bb8ea7ef401
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2011-04-20 14:08:47 +0200

    Tune test

--- yaff/test/test_md.py
+++ yaff/test/test_md.py
@@ -89,7 +89,7 @@ def get_ff_water32(do_valence=False, do_lj=False, do_eireal=False, do_eireci=Fal
 
 def test_gpos_water32_full():
     ff = get_ff_water32(True, True, True, True)
-    check_gpos_ff(ff, 1e-10)
+    check_gpos_ff(ff, 1e-8)
 
 def test_md_water32_full():
     dump = False
