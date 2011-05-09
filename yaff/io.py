dee69213b88b56700cc6480dd491fbe9f5807aeb
Louis Vanduyfhuys <Louis.Vanduyfhuys@UGent.be>
2011-05-09 13:26:29 +0200

    Added optimizer.py and io.py

---
+++ yaff/io.py
@@ -0,0 +1,232 @@
+# YAFF is yet another force-field code
+# Copyright (C) 2008 - 2011 Toon Verstraelen <Toon.Verstraelen@UGent.be>, Center
+# for Molecular Modeling (CMM), Ghent University, Ghent, Belgium; all rights
+# reserved unless otherwise stated.
+#
+# This file is part of YAFF.
+#
+# YAFF is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# YAFF is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>
+#
+# --
+
+from molmod.units import *
+from molmod.periodic import periodic
+import sys
+
+from yaff.ff import *
+from yaff.ext import *
+
+header = """******************************************************************************************************************************************************
+******************************************************************************************************************************************************
+***********************************                                                                               ************************************
+**********************************                          **    **   ****    ***  ***                            ***********************************
+*********************************                            **  **   ** **   **   **                               **********************************
+********************************                              ****   **  **   **   **                                *********************************
+********************************                               **   *******  **** ****                               *********************************
+********************************                              **   **    **   **   **                                *********************************
+*********************************                            **   **     **   **   **                               **********************************
+**********************************                          *************** ***  ***                               ***********************************
+***********************************                                                                               ************************************
+******************************************************************************************************************************************************
+******************************************************************************************************************************************************
+
+                                                   Welcome to YAFF - yet another force field code
+                                                Developed at the Center for Molecular Modeling (CMM)
+                                                           University of Ghent - Belgium
+                                                                 (C) Copyright 2011
+
+
+Execution date:             %s
+Machine:                    %s
+User:                       %s
+
+Input xyz:                  %s
+Input psf:                  %s
+Input valence parameters:   %s
+"""
+
+
+tail = """
+Terminated at:              %s
+
+******************************************************************************************************************************************************
+***********************************                    END OF FILE - THANK YOU, COME AGAIN ...                    ************************************
+******************************************************************************************************************************************************
+"""
+
+def print_block(title,block=None,out=sys.stdout):
+    print >> out, "~"*150
+    print >> out, " %s:" %title
+    print >> out, " "+"-"*(len(title)+1)
+    print >> out, ""
+    if block is not None:
+        print >> out, block
+        print >> out, ""
+
+
+def atom_attr_str(ff):
+    """
+        Write atom attributes (symbol, fftype, charge, vdW parameters) to string.
+    """
+    system = ff.system
+    vpart, pair_part_lj, pair_part_ei, ewald_reci_part, ewald_corr_part = ff.parts
+    charges = ewald_reci_part.charges
+    sigmas = pair_part_lj.pair_pot.sigmas
+    epsilons = pair_part_lj.pair_pot.epsilons
+    atom_attr = ""
+    atom_attr += "     number  atom   type     charge    sigma [A]    epsilon [kcalmol] \n"
+    atom_attr += "    ------------------------------------------------------------------\n"
+    for i in xrange(ff.system.natom):
+        atom_attr += "     %3i      %2s    %4s    % 5.4f     %6.4f          %7.5f   \n" \
+            %(i, periodic[system.numbers[i]].symbol, system.ffatypes[i], charges[i], sigmas[i]/angstrom, epsilons[i]/kcalmol)
+    return atom_attr
+
+
+def terms_str(ff):
+    """
+        Write force field terms to string.
+    """
+    system = ff.system
+    types = system.ffatypes
+    vpart, pair_part_lj, pair_part_ei, ewald_reci_part, ewald_corr_part = ff.parts
+    vlist = vpart.vlist.vtab
+    iclist = vpart.vlist.iclist.ictab
+    dlist = vpart.vlist.iclist.dlist.deltas
+
+    vkinds = ['Harmonic', 'PolyFour', 'Cross']
+    eunit = kjmol
+    ickinds = ['Bond', 'BendCos', 'BendAngle', 'DihedCos', 'DihedAngle']
+    icunits = [angstrom, 1.0, deg, 1.0, deg]
+    v_descr = ""
+    ic0_descr = ""
+    ic1_descr = ""
+
+    def get_ic_description(ic):
+        indexes = ""
+        d0 = dlist[ic['i0']]
+        atoms = (d0['i'], d0['j'])
+        if ic['kind'] > 0:
+            d1 = dlist[ic['i1']]
+            if d1['i']==atoms[0]:
+                atoms = (d1['j'], atoms[0], atoms[1])
+            elif d1['i']==atoms[1]:
+                atoms = (atoms[0], atoms[1], d1['j'])
+            elif d1['j']==atoms[0]:
+                atoms = (d1['i'], atoms[0], atoms[1])
+            elif d1['j']==atoms[1]:
+                atoms = (atoms[0], atoms[1], d1['i'])
+            else:
+                raise ValueError("Error in reading bend.")
+        if ic['kind'] > 2:
+            d2 = dlist[ic['i2']]
+            if d2['i']==atoms[0]:
+                atoms = (d2['j'], atoms[0], atoms[1], atoms[2])
+            elif d2['i']==atoms[2]:
+                atoms = (atoms[0], atoms[1], atoms[2], d2['j'])
+            elif d2['j']==atoms[0]:
+                atoms = (d2['i'], atoms[0], atoms[1], atoms[2])
+            elif d2['j']==atoms[2]:
+                atoms = (atoms[0], atoms[1], atoms[2], d2['i'])
+            else:
+                raise ValueError("Error in reading dihedral.")
+        for atom in atoms:
+            indexes += "%4s[%2i], " %(types[atom], atom)
+        indexes.rstrip(",")
+        return ickinds[ic['kind']], indexes
+
+    def get_par_description(term):
+        ic0unit = icunits[iclist[term['ic0']]['kind']]
+        if term['kind'] == 0:
+            unit_rv = ic0unit
+            if iclist[term['ic0']]['kind']==2:
+                unit_fc = eunit
+            else:
+                unit_fc = eunit/ic0unit**2
+            return "fc = %7.3f , rv = %7.3f" %(
+                term['par0']/unit_fc,
+                term['par1']/unit_rv
+            )
+        elif term['kind'] == 1:
+            return "a0 = % 7.3f , a1 = % 7.3f , a2 = % 7.3f , a3 = % 7.3f" %(
+                term['par0']/(eunit/ic0unit),
+                term['par1']/(eunit/ic0unit**2),
+                term['par2']/(eunit/ic0unit**3),
+                term['par3']/(eunit/ic0unit**4)
+            )
+        else:
+            raise NotImplementedError()
+
+    terms  = ""
+    terms += "       i  |   term   |     ic     |                   atoms                  |        pars [A, deg, kjmol/A^2, kjmol/rad^2, kjmol]              \n"
+    terms += "    --------------------------------------------------------------------------------------------------------------------------------------------\n"
+    for i in xrange(vpart.vlist.nv):
+        term = vlist[i]
+        ic0 = iclist[term['ic0']]
+        ic0_kind, ic0_indexes = get_ic_description(ic0)
+        if term['kind'] < 2:
+            par_descr = get_par_description(term)
+            terms += "     %3i  | %8s | %10s | %40s |  %s\n" %(i, vkinds[term['kind']], ic0_kind, ic0_indexes, par_descr)
+        else:
+            raise NotImplementedError()
+    return terms
+
+
+def ff_str(ff):
+    """
+        Write general force field info to string.
+    """
+    ff_info = ""
+    do_valence = False
+    do_ewald = False
+    do_ei_real = False
+    do_lj_real = False
+    for part in ff.parts:
+        if isinstance(part,ValencePart):
+            do_valence = True
+            vpart = part
+        if isinstance(part,EwaldReciprocalPart):
+            do_ewald = True
+            ewaldpart = part
+        if isinstance(part,PairPart):
+            if isinstance(part.pair_pot,PairPotEI):
+                do_ei_real = True
+                eipart = part
+            if isinstance(part.pair_pot,PairPotLJ):
+                do_lj_real = True
+                ljpart = part
+    if do_valence:
+        ff_info += "    Valence interactions: ON\n"
+    if do_ei_real:
+        ff_info += "    Real electrostatics: ON\n"
+        ff_info += "        cutoff      [A] = %9.6f\n" %(eipart.pair_pot.rcut/angstrom)
+        ff_info += "        1-2 scaling [ ] = %4.3f\n" %eipart.scalings.scale1
+        ff_info += "        1-3 scaling [ ] = %4.3f\n" %eipart.scalings.scale2
+        ff_info += "        1-4 scaling [ ] = %4.3f\n" %eipart.scalings.scale3
+    else:
+        ff_info += "    Real electrostatics: OFF\n"
+    if do_ewald:
+        ff_info += "    Reciprocal electrostatics: EWALD\n"
+        ff_info += "        alpha [1/A] = %9.6f\n" %(ewaldpart.alpha*angstrom)
+        ff_info += "        gmax  [   ] = %2i,%2i,%2i\n" %(ewaldpart.gmax[0],ewaldpart.gmax[1],ewaldpart.gmax[2])
+    else:
+        ff_info += "    Reciprocal electrostatics: OFF\n"
+    if do_lj_real:
+        ff_info += "    Real lennard-jones: ON\n"
+        ff_info += "        cutoff      [A] = %9.6f\n" %(ljpart.pair_pot.rcut/angstrom)
+        ff_info += "        1-2 scaling [ ] = %4.3f\n" %ljpart.scalings.scale1
+        ff_info += "        1-3 scaling [ ] = %4.3f\n" %ljpart.scalings.scale2
+        ff_info += "        1-4 scaling [ ] = %4.3f\n" %ljpart.scalings.scale3
+    else:
+        ff_info += "    Real lennard-jones: OFF\n"
+    return ff_info
---
+++ yaff/optimizer.py
@@ -0,0 +1,356 @@
+# YAFF is yet another force-field code
+# Copyright (C) 2008 - 2011 Toon Verstraelen <Toon.Verstraelen@UGent.be>, Center
+# for Molecular Modeling (CMM), Ghent University, Ghent, Belgium; all rights
+# reserved unless otherwise stated.
+#
+# This file is part of YAFF.
+#
+# YAFF is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# YAFF is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>
+#
+# --
+
+import sys, numpy as np
+from molmod.minimizer import *
+from molmod.units import *
+from molmod.io.xyz import XYZWriter
+from molmod.periodic import periodic as periodictable
+
+__all__ = [
+    'Optimizer', 'CellOptimizer', 'ThetaOptimizer',
+]
+
+class Optimizer(object):
+    """
+        An abstract Optimizer class.
+    """
+    def __init__(self, ff, rvecs_scale = 1e-2,
+            search_direction = ConjugateGradient(), line_search = NewtonLineSearch(), convergence = ConvergenceCondition(grad_rms=1e-4, grad_max=3e-4),
+            stop_loss = StopLossCondition(max_iter=1000), xyz_writer=None, out=None):
+        self.ff = ff
+        self.rvecs_scale = rvecs_scale
+        self.periodic = ff.system.cell.rvecs.shape == (3,3)
+        self.search_direction = search_direction
+        self.line_search = line_search
+        self.convergence = convergence
+        self.stop_loss = stop_loss
+
+        if out is None:
+            out = file("yaff.out", 'w')
+        else:
+            if isinstance(out, file):
+                self.out = out
+            elif isinstance(out, str):
+                self.out = file(out, 'w')
+            else:
+                raise TypeError("argument out should be of type string or file")
+
+        if xyz_writer is None:
+            self.xyz_writer = XYZWriter(
+                file("yaff-traj.xyz", 'w'),
+                [periodictable[i].symbol for i in ff.system.numbers]
+            )
+        else:
+            if isinstance(xyz_writer, file):
+                self.xyz_writer =  XYZWriter(
+                    xyz_writer,
+                    [periodictable[i].symbol for i in ff.system.numbers]
+                )
+            elif isinstance(xyz_writer, str):
+                self.xyz_writer = XYZWriter(
+                    file(xyz_writer, 'w'),
+                    [periodictable[i].symbol for i in ff.system.numbers]
+                )
+            elif isinstance(xyz_writer, XYZWriter):
+                self.xyz_writer = xyz_writer
+            else:
+                raise TypeError("argument xyz_writer should be of type file, string or XYZWriter")
+
+
+    def get_xinit(self, *args):
+        raise NotImplementedError
+
+    def calc_energy(self, x, do_gradient=False):
+        raise NotImplementedError
+
+    def callback(self, m):
+        raise NotImplementedError
+
+    def write_results(self, *args):
+        raise NotImplementedError
+
+    def run(self):
+        self.minimizer = Minimizer(
+            self.get_xinit(), self.calc_energy, self.search_direction, self.line_search,
+            self.convergence, self.stop_loss, anagrad=True, verbose=True, callback=self.callback
+        )
+
+
+class CellOptimizer(Optimizer):
+    """
+        An optimizer for performing geometrical optimization of orthorhombic cells in which all cell vector lengths are allowed to change.
+    """
+    def __init__(self, ff, rvecs_scale = 1e-2, search_direction=ConjugateGradient(), line_search=NewtonLineSearch(),
+            convergence=ConvergenceCondition(grad_rms=1e-4, grad_max=3e-4), stop_loss=StopLossCondition(max_iter=1000),
+            xyz_writer=None, out=None):
+        Optimizer.__init__(self, ff, rvecs_scale=rvecs_scale, search_direction=search_direction, line_search=line_search,
+            convergence=convergence, stop_loss=stop_loss, xyz_writer=xyz_writer, out=out)
+        print >> self.out, ""
+        print >> self.out, "  Geometry Optimization:"
+        print >> self.out, "  ----------------------"
+        print >> self.out, ""
+        print >> self.out, "      i  |  Energy [kcalmol]  |   a [A]   b [A]    c [A]"
+        print >> self.out, "    ----------------------------------------------------"
+
+    def get_xinit(self):
+        if self.periodic:
+            x_init = np.concatenate([
+                np.diag(self.ff.system.cell.rvecs)*self.rvecs_scale,
+                np.dot(self.ff.system.pos, np.linalg.inv(self.ff.system.cell.rvecs)).ravel(),
+            ])
+        else:
+            x_init = self.ff.system.pos.ravel()
+        return x_init
+
+    def calc_energy(self, x, do_gradient=False):
+        if self.periodic:
+            rvecs = np.diag(x[:3])/self.rvecs_scale
+            scaled = x[3:].reshape(-1,3)
+            pos = np.dot(scaled, rvecs)
+            self.ff.update_rvecs(rvecs)
+        else:
+            pos = x.reshape(-1,3)
+        self.ff.update_pos(pos)
+        if do_gradient:
+            gpos = np.zeros(pos.shape)
+            vtens = np.zeros((3, 3), float)
+            energy = self.ff.compute(gpos, vtens)
+            if self.periodic:
+                grad_rvecs = np.dot(self.ff.system.cell.gvecs, vtens)
+                grad_scaled = np.dot(gpos, rvecs)
+                gradient = np.concatenate([
+                    np.diag(grad_rvecs)/self.rvecs_scale,
+                    grad_scaled.ravel()
+                ])
+            else:
+                gradient = gpos.ravel()
+            return energy, gradient
+        else:
+            energy = self.ff.compute()
+            return energy
+
+    def callback(self, m):
+        if self.periodic:
+            rvecs = np.diag(m.x[:3])/self.rvecs_scale
+            scaled = m.x[3:].reshape(-1,3)
+            pos = np.dot(scaled, rvecs)
+            self.xyz_writer.dump(
+                'Energy = %.10f , Cell = %f,%f,%f' % (m.f, rvecs[0,0]/angstrom, rvecs[1,1]/angstrom, rvecs[2,2]/angstrom),
+                pos
+            )
+            print >> self.out, "     %3i |    %10.6f    | % 5.3f  % 5.3f  % 5.3f " %(
+                m.counter,
+                m.f/kcalmol,
+                self.ff.system.cell.rvecs[0,0]/angstrom,
+                self.ff.system.cell.rvecs[1,1]/angstrom,
+                self.ff.system.cell.rvecs[2,2]/angstrom,
+            )
+        else:
+            pos = m.x.reshape(-1,3)
+            self.xyz_writer.dump('Energy = %.10f' % m.f, pos)
+
+    def write_results(self, xyz_writer=None, ener_writer=None, vdw_writer=None):
+        # write energy to energy file
+        energy = self.ff.compute()
+        if ener_writer is not None:
+            ener_writer.write("  % .10f\n" % energy)
+
+        # write vdw contribution to vdw file
+        if vdw_writer is not None:
+            lj_part = None
+            for part in self.ff.parts:
+                if isinstance(part, PairPart):
+                    if isinstance(part.pair_pot,PairPotLJ):
+                        lj_part = part
+            vdw = lj_part._internal_compute(None, None)
+            vdw_writer.write("  %.10f\n" % vdw)
+
+        # write xyz to trajectory file
+        if xyz_writer is not None:
+            xyz_writer.dump('Energy  = %.10f' % energy, self.ff.system.pos)
+
+        # write to general output
+        print >> self.out, ""
+        if self.minimizer.success:
+            print >> self.out, " Final Geometry (CONVERGED)"
+        else:
+            print >> self.out, " Final Geometry (NOT CONVERGED)"
+
+        print >> self.out, " ----------------------------------"
+        print >> self.out, ""
+        print >> self.out, "    Energy [kcalmol]= %15.9f" %(self.ff.compute()/kcalmol)
+        print >> self.out, "    Cell dimensions:"
+        print >> self.out, "       a [A] = %9.6f %9.6f %9.6f" %(
+            self.ff.system.cell.rvecs[0,0]/angstrom, self.ff.system.cell.rvecs[0,1]/angstrom, self.ff.system.cell.rvecs[0,2]/angstrom
+        )
+        print >> self.out, "       b [A] = %9.6f %9.6f %9.6f" %(
+            self.ff.system.cell.rvecs[1,0]/angstrom, self.ff.system.cell.rvecs[1,1]/angstrom, self.ff.system.cell.rvecs[1,2]/angstrom
+        )
+        print >> self.out, "       c [A] = %9.6f %9.6f %9.6f" %(
+            self.ff.system.cell.rvecs[2,0]/angstrom, self.ff.system.cell.rvecs[2,1]/angstrom, self.ff.system.cell.rvecs[2,2]/angstrom
+        )
+        print >> self.out, "       cell volume          [A^3] = %10.6f" %(self.ff.system.cell.volume/angstrom**3)
+        print >> self.out, "       cell diagonal length   [A] = %9.6f" %(np.sqrt(self.ff.system.cell.rvecs[0,0]**2 + self.ff.system.cell.rvecs[2,2]**2)/angstrom)
+        print >> self.out, "       cell diagonal angle  [deg] = %9.6f" %(2.0*np.arctan(self.ff.system.cell.rvecs[2,2]/self.ff.system.cell.rvecs[0,0])/deg)
+        print >> self.out, ""
+        print >> self.out, "~"*150
+
+
+class ThetaOptimizer(Optimizer):
+    """
+        An optimizer for performing geometrical optimization at a fixed angle of the diagonal between a and c cell vectors.
+        The length of the diagonal and the length of the b cell vector is allowed to change during optimization.
+    """
+    def __init__(self, ff, theta, rvecs_scale = 1e-2, search_direction=ConjugateGradient(), line_search=NewtonLineSearch(),
+            convergence=ConvergenceCondition(grad_rms=1e-4, grad_max=3e-4), stop_loss=StopLossCondition(max_iter=1000),
+            xyz_writer=None, out=None):
+        Optimizer.__init__(self, ff, rvecs_scale=rvecs_scale, search_direction=search_direction, line_search=line_search,
+            convergence=convergence, stop_loss=stop_loss, xyz_writer=xyz_writer, out=out)
+        self.theta = theta
+        print >> self.out, ""
+        print >> self.out, "  Geometry Optimization at theta = %6.3f deg:" %(self.theta/deg)
+        print >> self.out, "  ---------------------------------------------"
+        print >> self.out, ""
+        print >> self.out, "      i  |  Energy [kcalmol]  |   d [A]   b [A]"
+        print >> self.out, "    -------------------------------------------"
+
+    def get_xinit(self):
+        if self.periodic:
+            x_init = np.concatenate([
+                np.array([
+                    np.sqrt(self.ff.system.cell.rvecs[0,0]**2 + self.ff.system.cell.rvecs[2,2]**2),
+                    self.ff.system.cell.rvecs[1,1]
+                ])*self.rvecs_scale,
+                np.dot(
+                    self.ff.system.pos,
+                    np.linalg.inv(self.ff.system.cell.rvecs)
+                ).ravel(),
+            ])
+        else:
+            x_init = self.ff.system.pos.ravel()
+        return x_init
+
+    def calc_energy(self, x, do_gradient=False):
+        if self.periodic:
+            rvecs = np.diag([
+                x[0]*np.cos(self.theta/2.0),
+                x[1],
+                x[0]*np.sin(self.theta/2.0),
+            ])/self.rvecs_scale
+            scaled = x[2:].reshape(-1,3)
+            pos = np.dot(scaled, rvecs)
+            self.ff.update_rvecs(rvecs)
+        else:
+            pos = x.reshape(-1,3)
+        self.ff.update_pos(pos)
+
+        if do_gradient:
+            gpos = np.zeros(self.ff.system.pos.shape)
+            vtens = np.zeros((3, 3), float)
+            energy = self.ff.compute(gpos, vtens)
+            if self.periodic:
+                grad_rvecs = np.dot(self.ff.system.cell.gvecs, vtens)
+                grad_scaled = np.dot(gpos, rvecs)
+                gradient = np.concatenate([
+                    np.array([
+                        grad_rvecs[0,0]*np.cos(self.theta/2.0) + grad_rvecs[2,2]*np.sin(self.theta/2.0),
+                        grad_rvecs[1,1],
+                    ])/self.rvecs_scale,
+                    grad_scaled.ravel()
+                ])
+            else:
+                gradient = gpos.ravel()
+            return energy, gradient
+        else:
+            energy = self.ff.compute()
+            return energy
+
+    def callback(self, m):
+        if self.periodic:
+            d, b = m.x[:2]/self.rvecs_scale
+            rvecs = np.diag(np.array([
+                d*np.cos(self.theta/2.0),
+                b,
+                d*np.sin(self.theta/2.0),
+            ]))
+            scaled = m.x[2:].reshape(-1,3)
+            pos = np.dot(scaled, rvecs)
+            self.xyzwriter.dump(
+                'Energy = %.10f , d = %6.3f , b = %6.3f' % (m.f, d, b),
+                pos
+            )
+            print >> self.out, "     %3i |    %10.6f    | % 5.3f  % 5.3f  " %(
+                m.counter,
+                m.f/kcalmol,
+                d/angstrom,
+                b/angstrom,
+            )
+        else:
+            pos = m.x.reshape(-1,3)
+            self.xyzwriter.dump('Energy = %.10f' % m.f, pos)
+
+    def write_results(self, index="", xyz_writer=None, ener_writer=None, vdw_writer=None):
+        # write energy to energy file
+        energy = self.ff.compute()
+        if ener_writer is not None:
+            ener_writer.write("  %.10f   % .10f\n" %(theta, energy))
+
+        # write vdw contribution to vdw file
+        if vdw_writer is not None:
+            lj_part = None
+            for part in self.ff.parts:
+                if isinstance(part, PairPart):
+                    if isinstance(part.pair_pot,PairPotLJ):
+                        lj_part = part
+            vdw = lj_part._internal_compute(None, None)
+            vdw_writer.write("  %.10f   % .10f\n" %(theta, vdw))
+
+        # write xyz to trajectory file
+        if xyz_writer is not None:
+            xyz_writer.dump('Theta[%s]  = %.10f, Energy  = %.10f' % (str(index), self.theta, energy), self.ff.system.pos)
+
+        # write to general output
+        print >> self.out, ""
+        if self.minimizer.success:
+            print >> self.out, " Geometry at theta[%s] = %6.3f deg (CONVERGED)" %(str(index), self.theta/deg)
+        else:
+            print >> self.out, " Geometry at theta[%s] = %6.3f deg (NOT CONVERGED)" %(str(index), self.theta/deg)
+
+        print >> self.out, " ----------------------------------"
+        print >> self.out, ""
+        print >> self.out, "    Energy [kcalmol]= %15.9f" %(self.ff.compute()/kcalmol)
+        print >> self.out, "    Cell dimensions:"
+        print >> self.out, "       a [A] = %9.6f %9.6f %9.6f" %(
+            self.ff.system.cell.rvecs[0,0]/angstrom, self.ff.system.cell.rvecs[0,1]/angstrom, self.ff.system.cell.rvecs[0,2]/angstrom
+        )
+        print >> self.out, "       b [A] = %9.6f %9.6f %9.6f" %(
+            self.ff.system.cell.rvecs[1,0]/angstrom, self.ff.system.cell.rvecs[1,1]/angstrom, self.ff.system.cell.rvecs[1,2]/angstrom
+        )
+        print >> self.out, "       c [A] = %9.6f %9.6f %9.6f" %(
+            self.ff.system.cell.rvecs[2,0]/angstrom, self.ff.system.cell.rvecs[2,1]/angstrom, self.ff.system.cell.rvecs[2,2]/angstrom
+        )
+        print >> self.out, "       cell volume          [A^3] = %10.6f" %(self.ff.system.cell.volume/angstrom**3)
+        print >> self.out, "       cell diagonal length   [A] = %9.6f" %(np.sqrt(self.ff.system.cell.rvecs[0,0]**2 + self.ff.system.cell.rvecs[2,2]**2)/angstrom)
+        print >> self.out, "       cell diagonal angle  [deg] = %9.6f" %(2.0*np.arctan(self.ff.system.cell.rvecs[2,2]/self.ff.system.cell.rvecs[0,0])/deg)
+        print >> self.out, ""
+        print >> self.out, "~"*150
