626c7151c1cdb2d7f3ceec617fc70dfe2440c00c
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2011-10-02 19:06:07 -0400

    More cleaning

--- cleanfiles.sh
+++ cleanfiles.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 for i in $(find yaff | egrep "\.pyc$|\.py~$|\.pyc~$|\.bak$|\.so$") ; do rm -v ${i}; done
 
-rm -v yaff/ext.c
+rm -v yaff/pes/ext.c
+rm -rv doc/_build
 rm -v MANIFEST
 rm -vr dist
 rm -vr build
