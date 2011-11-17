c2ac95e29d0ca4e635751cc094de3b662a7f5812
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2011-11-17 17:05:03 -0500

    Improve cleancode script

--- cleancode.sh
+++ cleancode.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo Cleaning code in \'`pwd`\' and subdirectories
-for file in `find yaff *.c *.py doc | egrep "(\.py$)|(\.c$)|(\.h$)|(\.pyx$)|(\.pxd$)|(\.rst$)|(\.txt$)"`; do
+for file in `find yaff input *.c *.py doc | egrep "(\.py$)|(\.c$)|(\.h$)|(\.pyx$)|(\.pxd$)|(\.rst$)|(\.txt$)"`; do
   echo Cleaning ${file}
   sed -i -e $'s/\t/    /' ${file}
   sed -i -e $'s/[ \t]\+$//' ${file}
