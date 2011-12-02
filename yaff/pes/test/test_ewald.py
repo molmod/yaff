Fix some scaling issues in the tests

yaff/pes/test/test_ewald.py
@@ -30,19 +30,13 @@ from yaff.test.common import get_system_water32, get_system_quartz
 from yaff.pes.test.common import check_gpos_part, check_vtens_part
 
 
-# TODO: Switcv to supercells for the quartz tests, such that completely random
-# scalings can be used. It would be good to have a general supercell method
-# in the system class.
-
-
 def test_ewald_water32():
     system = get_system_water32()
     check_alpha_depedence(system)
 
 
 def test_ewald_quartz():
-    raise SkipTest('This test should use a supercell to avoid scaling troubles.')
-    system = get_system_quartz()
+    system = get_system_quartz().supercell(2, 2, 2)
     check_alpha_depedence(system)
 
 
@@ -126,9 +120,9 @@ def test_ewald_reci_volchange_quartz():
 
 def test_ewald_corr_quartz():
     from scipy.special import erf
-    system = get_system_quartz()
+    system = get_system_quartz().supercell(2, 2, 2)
     for alpha in 0.05, 0.1, 0.2:
-        scalings = Scalings(system, np.random.uniform(0.1, 0.9), 1.0, 1.0)
+        scalings = Scalings(system, np.random.uniform(0.1, 0.9), np.random.uniform(0.1, 0.9), np.random.uniform(0.1, 0.9))
         part_ewald_corr = ForcePartEwaldCorrection(system, alpha, scalings)
         energy1 = part_ewald_corr.compute()
         # self-interaction corrections
@@ -153,8 +147,8 @@ def test_ewald_gpos_vtens_corr_water32():
 
 
 def test_ewald_gpos_vtens_corr_quartz():
-    system = get_system_quartz()
-    scalings = Scalings(system, np.random.uniform(0.1, 0.9), 1.0, 1.0)
+    system = get_system_quartz().supercell(2, 2, 2)
+    scalings = Scalings(system, np.random.uniform(0.1, 0.9), np.random.uniform(0.1, 0.9), np.random.uniform(0.1, 0.9))
     for alpha in 0.1, 0.2, 0.5:
         part_ewald_corr = ForcePartEwaldCorrection(system, alpha, scalings)
         check_gpos_part(system, part_ewald_corr)

yaff/pes/test/test_scaling.py
@@ -60,7 +60,7 @@ def test_scaling_glycine():
 
 
 def test_scaling_quartz():
-    system = get_system_quartz()
+    system = get_system_quartz().supercell(2, 2, 2)
     stab = Scalings(system).stab
     assert (stab['a'] > stab['b']).all()
     assert len(stab) == sum(len(system.neighs1[i]) + len(system.neighs2[i]) for i in xrange(system.natom))/2
@@ -96,6 +96,9 @@ def test_iter_paths3():
 
 def test_scaling_mil53():
     system = get_system_mil53()
-    from molmod import angstrom
-    print np.linalg.norm(system.pos[0] - system.pos[5])/angstrom
-    scalings = Scalings(system)
+    try:
+        scalings = Scalings(system)
+        success = False
+    except AssertionError:
+        success = True
+    assert success