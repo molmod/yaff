15e873be31ba9649b05a4b21933142ae53e52644
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2012-08-28 15:28:21 +0200

    Improved cleanfiles.sh

--- cleanfiles.sh
+++ cleanfiles.sh
@@ -3,6 +3,7 @@ for i in $(find yaff examples | egrep "\.pyc$|\.py~$|\.pyc~$|\.bak$|\.so$") ; do
 
 for d in examples/*; do (cd $d; ./clean.sh); done
 
+rm -vr doctrees
 rm -v yaff/pes/ext.c
 rm -rv doc/_build
 rm -v MANIFEST
