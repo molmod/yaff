a960b536ce4d4e2abb3981215fbd178288c61132
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2014-11-29 11:27:19 +0100

    Fix in cleanfiles.sh

--- cleanfiles.sh
+++ cleanfiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-for i in $(find yaff examples | egrep "\.pyc$|\.py~$|\.pyc~$|\.bak$|\.so$") ; do rm -v ${i}; done
+for i in $(find yaff data/examples | egrep "\.pyc$|\.py~$|\.pyc~$|\.bak$|\.so$") ; do rm -v ${i}; done
 
-for d in data/examples/*; do (cd $d; ./clean.sh); done
+for d in data/examples/*; do (cd $d; echo Claning in $d; ./clean.sh); done
 
 rm -vr doctrees
 rm -v yaff/pes/ext.c
