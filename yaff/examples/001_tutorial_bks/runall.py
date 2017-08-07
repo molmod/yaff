51dc7190f67340c0292445305c405de741795e59
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2017-08-07 09:52:04 +0200

    Better subprocess call in runall.py

--- yaff/examples/001_tutorial_bks/runall.py
+++ yaff/examples/001_tutorial_bks/runall.py
@@ -2,8 +2,8 @@
 
 import subprocess
 
-subprocess.check_output(["python", "mksystem.py"], cwd='init')
-subprocess.check_output(["python", "simulation.py"], cwd='opt')
-subprocess.check_output(["python", "analysis.py"], cwd='opt')
-subprocess.check_output(["python", "simulation.py", "300", "310"], cwd='nvt')
-subprocess.check_output(["python", "analysis.py", "300", "310", "10"], cwd='nvt')
+subprocess.check_call(["python", "mksystem.py"], cwd='init')
+subprocess.check_call(["python", "simulation.py"], cwd='opt')
+subprocess.check_call(["python", "analysis.py"], cwd='opt')
+subprocess.check_call(["python", "simulation.py", "300", "310"], cwd='nvt')
+subprocess.check_call(["python", "analysis.py", "300", "310", "10"], cwd='nvt')
