612d71c050a085a8b63aa86a5f08bfe375477de4
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2017-08-29 15:02:58 +0200

    Also take run_with_env from molmod

--- tools/appveyor/run_with_env.cmd
+++ tools/appveyor/run_with_env.cmd
@@ -32,18 +32,16 @@ IF %MAJOR_PYTHON_VERSION% == "2" (
     EXIT 1
 )
 
-IF "%PLATFORM%"=="x64" (
+IF "%PLATFORM%"=="X64" (
     ECHO Configuring Windows SDK %WINDOWS_SDK_VERSION% for Python %MAJOR_PYTHON_VERSION% on a 64 bit architecture
     SET DISTUTILS_USE_SDK=1
     SET MSSdk=1
     "%WIN_SDK_ROOT%\%WINDOWS_SDK_VERSION%\Setup\WindowsSdkVer.exe" -q -version:%WINDOWS_SDK_VERSION%
     "%WIN_SDK_ROOT%\%WINDOWS_SDK_VERSION%\Bin\SetEnv.cmd" /x64 /release
-    CALL "C:\Program Files\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
     ECHO Executing: %COMMAND_TO_RUN%
     call %COMMAND_TO_RUN% || EXIT 1
 ) ELSE (
-    ECHO Using MSVC 14.0 for 32 bit architecture
-    CALL "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+    ECHO Using default MSVC build environment for 32 bit architecture
     ECHO Executing: %COMMAND_TO_RUN%
     call %COMMAND_TO_RUN% || EXIT 1
 )
