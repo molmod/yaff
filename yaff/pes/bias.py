5944433048e9466fa1fa1b72abbbfbf5c050a2c5
Steven Vandenbrande <Steven.Vandenbrande@UGent.be>
2019-06-13 14:12:23 +0200

    Add BiasPotentials

---
+++ yaff/pes/bias.py
@@ -0,0 +1,311 @@
+# -*- coding: utf-8 -*-
+# YAFF is yet another force-field code.
+# Copyright (C) 2011 Toon Verstraelen <Toon.Verstraelen@UGent.be>,
+# Louis Vanduyfhuys <Louis.Vanduyfhuys@UGent.be>, Center for Molecular Modeling
+# (CMM), Ghent University, Ghent, Belgium; all rights reserved unless otherwise
+# stated.
+#
+# This file is part of YAFF.
+#
+# YAFF is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# YAFF is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>
+#
+# --
+'''Bias potentials'''
+
+
+from __future__ import division
+
+import numpy as np
+
+from yaff.log import log, timer
+
+
+__all__ = [
+    'BiasPotential', 'HarmonicBias', 'PathDeviationBias'
+]
+
+
+class BiasPotential(object):
+    '''Base class for energy terms used to bias the system.'''
+    def __init__(self, pars, cvs):
+        '''
+           **Arguments:**
+
+           pars
+                A list of parameters to be stored for this energy term.
+
+           cvs
+                A list of ``CollectiveVariables``
+        '''
+        self.pars = pars
+        self.cvs = cvs
+
+    def get_log(self):
+        '''Describe the covalent energy term in a format that is suitable for
+           screen logging.
+        '''
+        raise NotImplementedError
+
+    def compute(self, gpos=None, vtens=None):
+        """Compute the bias potential and optionally some derivatives
+
+           The only variable inputs for the compute routine are the atomic
+           positions and the cell vectors.
+
+           **Optional arguments:**
+
+           gpos
+                The derivatives of the collective towards the Cartesian
+                coordinates of the atoms. ('g' stands for gradient and 'pos'
+                for positions.)
+                This must be a writeable numpy array with shape (N, 3) where N
+                is the number of atoms.
+
+           vtens
+                The force contribution to the pressure tensor. This is also
+                known as the virial tensor. It represents the derivative of the
+                energy towards uniform deformations, including changes in the
+                shape of the unit cell. (v stands for virial and 'tens' stands
+                for tensor.) This must be a writeable numpy array with shape (3,
+                3).
+
+           The collective variable value is returned. The optional arguments
+           are Fortran-style output arguments. When they are present, the
+           corresponding results are computed and **stored** to the current
+           contents of the array.
+        """
+        #Subclasses implement their compute code here.
+        raise NotImplementedError
+
+
+class HarmonicBias(BiasPotential):
+    '''The harmonic energy term: 0.5*K*(q-q0)^2'''
+    def __init__(self, fc, rv, cv):
+        '''
+           **Arguments:**
+
+           fc
+                The force constant (in atomic units).
+
+           rv
+                The rest value (in atomic units).
+
+           cv
+                An ``CollectiveVariable`` object.
+        '''
+        BiasPotential.__init__(self, [fc, rv], [cv])
+
+    def get_log(self):
+        c = self.cvs[0].get_conversion()
+        return '%s(FC=%.5e,RV=%.5e)' % (
+            self.__class__.__name__,
+            self.pars[0]/(log.energy.conversion/c**2),
+            self.pars[1]/c
+        )
+
+    def compute(self, gpos=None, vtens=None):
+        q = self.cvs[0].compute(gpos=gpos,vtens=vtens)
+        x = q-self.pars[1]
+        e = 0.5*self.pars[0]*x**2
+        if gpos is not None:
+            gpos[:] *= self.pars[0]*x
+        if vtens is not None:
+            vtens[:] *= self.pars[0]*x
+        return e
+
+
+class PathDeviationBias(BiasPotential):
+    '''
+    Given a discrete path in the multidimensional collective variable space,
+    this potential consists of the energy of the nearest point on the path plus
+    a harmonic potential for the distance to the nearest point.
+
+    Suppose the vectors of collective variables on the path are given by
+    sigma_i for i=1..n. Suppose the current state of the system is sigma. First
+    the index i is sought for which |sigma-sigma_i| is minimal. This expression
+    for this bias potential is then U=U_path + 0.5*K*|sigma-sigma_i|**2
+
+    U_path is computed as a three point interpolation:
+    U_path = c_{i-1}*U_{i-1}+c_{i}*U_{i}+c_{i+1}*U_{i+1} with the coefficients:
+        *c_{i} =   0.5*( d_{i-1}/(d_{i-1}+d_{i}) + d_{i+1}/(d_{i}+d_{i+1} )
+        *c_{i-1} = 0.5*( d_{i}/(d_{i-1}+d_{i}) )
+        *c_{i+1} = 0.5*( d_{i}/(d_{i}+d_{i+1}) )
+    where d_{i} = |sigma-sigma_i|**2
+
+    Note that this potential is **not** continuous when the nearest point on
+    the path changes from one to the other.
+
+    Defining a distance in collective variable space is problematic: different
+    collective variables can even have different units. We use the following
+    expression: |sigma| = \sqrt{ Sum_a (w_a sigma_a)**2 } where a runs over all
+    collective variables and w_a are weights provided by the user.
+    '''
+    def __init__(self, cvs, path, fc, weights=None, periodicities=None):
+        '''
+           **Arguments:**
+
+           cvs
+                A list of CollectiveVariables
+
+           path
+                Specification of the path, either a filename to text file or a
+                NumPy array. In either case, the first N columns provide the
+                collective variable coordinates of the path, with N the number
+                of collective variables. The next column provides the biasing
+                energy along the path.
+
+           fc
+                Force constant K appearing in the harmonic restraint
+
+           **Optional arguments:**
+
+           weights
+                Contribution of each collective variable when computing
+                distances in collective variable space, default is all equal to
+                1
+
+           periodicities
+                List containing the periodicity of each collective variable.
+                Specify ``None`` if the collective variable is not periodic.
+                Default is ``None`` for all collective variables.
+
+        TODO
+            Testing periodic boundary conditions
+            Testing the search for closest point on the path
+        '''
+        self.cvs = cvs
+        self.fc = fc
+        self.ncv = len(cvs)
+        if weights is None:
+            weights = np.ones((self.ncv))
+        if periodicities is None:
+            periodicities = [None]*self.ncv
+        assert weights.shape[0]==self.ncv
+        assert len(periodicities)==self.ncv
+        self.weights = weights
+        self.periodicities = periodicities
+        # Load the path from a txt file if a filename was passed
+        if isinstance(path, str):
+            path = np.loadtxt(path)
+        # Check that we now have a NumPy array of the correct shape
+        assert isinstance(path, np.ndarray)
+        assert path.shape[1]==self.ncv+1
+        self.path_coordinates = path[:,:self.ncv]
+        self.path_energies = path[:,self.ncv]
+        self.extended_energies = np.insert(self.path_energies, 0, 0.0)
+        self.extended_energies = np.append(self.extended_energies, 0.0)
+        self.npoints = self.path_energies.shape[0]
+        # Arrays that can hold derivatives for each collective variable
+        self.values_cvs = np.zeros((self.ncv,))
+        self.gpos_cvs = np.zeros((self.ncv, self.cvs[0].gpos.shape[0], self.cvs[0].gpos.shape[1]))
+        self.vtens_cvs = np.zeros((self.ncv, self.cvs[0].vtens.shape[0], self.cvs[0].vtens.shape[1]))
+
+    def get_log(self):
+        return '%s'%self.__class__.__name__
+
+    def find_nearest_point(self, x):
+        '''
+           Search the point on the path that is closest to x
+
+           **Arguments:**
+
+           x
+                A NumPy array specifying the values of the collective variables
+
+           **Returns:**
+
+           index
+                The index of the nearest point on the path
+
+           sqdists
+                the squares of the distances to the left, central, and right
+                points on the path. Central point is the nearest point.
+
+           deltas
+                the vectors connecting x with the left, central, and right
+                points on the path. Central point is the nearest point.
+        '''
+        assert x.ndim==1        
+        assert x.shape[0]==self.ncv
+        deltas = x-self.path_coordinates
+        # Apply minimum image convention for periodic collective variables
+        for icv, period in enumerate(self.periodicities):
+            if period is None: continue
+            deltas[:,icv] -= period*np.floor(deltas[:,icv]/period)
+        # Compute square of distances
+        sqdists = np.einsum('ia,a',deltas**2,self.weights**2)
+        index = np.argmin(sqdists)
+        if index==0:
+            sqdists = np.insert(sqdists[:index+2], 0, 0.0)
+            deltas = np.insert(deltas[:index+2], 0, [0.0]*self.ncv, axis=0)
+        elif index==self.npoints-1:
+            sqdists = np.append(sqdists[index-2:], 0.0)
+            deltas = np.append(deltas[index-2:], [0.0]*self.ncv, axis=0)
+        else:
+            sqdists = sqdists[index-1:index+2]
+            deltas = deltas[index-1:index+2]
+        assert sqdists.shape[0]==3
+        assert deltas.shape[0]==3
+        return index, sqdists, deltas
+
+    def compute(self, gpos=None, vtens=None):
+        # Compute collective variable values and optionally derivatives by
+        # looping over all collective variables
+        self.values_cvs[:] = 0.0
+        self.gpos_cvs[:] = 0.0
+        self.vtens_cvs[:] = 0.0
+        for icv in range(self.ncv):
+            if gpos is not None:
+                my_gpos = self.gpos_cvs[icv]
+            else: my_gpos = None
+            if vtens is not None:
+                my_vtens = self.vtens_cvs[icv]
+            else: my_vtens = None
+            self.values_cvs[icv] = self.cvs[icv].compute(gpos=my_gpos,vtens=my_vtens)
+        # Get the index of the nearest point on the path, the square of the
+        # distances, and the vectors connecting the path with the current point
+        index, sqdists, deltas = self.find_nearest_point(self.values_cvs)
+        energies = self.extended_energies[index:index+3]
+        # First contribution: interpolate the path energy using following
+        # coefficients
+        coeff = np.zeros((3))
+        coeff[0] = 0.5*sqdists[1]/(sqdists[0]+sqdists[1])
+        coeff[1] = 0.5*sqdists[0]/(sqdists[0]+sqdists[1])
+        coeff[1]+= 0.5*sqdists[2]/(sqdists[2]+sqdists[1])
+        coeff[2] = 0.5*sqdists[1]/(sqdists[2]+sqdists[1])
+        energy = np.sum(energies*coeff)
+        # Special cases: endpoints of the path, so interpolation is only
+        # between two points
+        if index in [0, self.npoints]: energy *= 2.0
+        if not ((gpos is None) and (vtens is None)):
+            path_derivatives = np.zeros((self.ncv,))
+            path_derivatives += (deltas[0]*sqdists[1]-deltas[1]*sqdists[0])*\
+                (energies[1]-energies[0])/(sqdists[0]+sqdists[1])**2
+            path_derivatives += (deltas[2]*sqdists[1]-deltas[1]*sqdists[2])*\
+                (energies[1]-energies[2])/(sqdists[2]+sqdists[1])**2
+            path_derivatives *= self.weights**2
+            if index in [0, self.npoints]: path_derivatives *= 2.0
+        # Derivatives towards atomic positions and cell tensor elements are
+        # computed using the chain rule
+        if gpos is not None:
+            gpos[:] = np.einsum('aij,a->ij', self.gpos_cvs, path_derivatives)
+        if vtens is not None:
+            vtens[:] = np.einsum('aij,a->ij', self.vtens_cvs, path_derivatives)
+        # Second contribution: harmonic restraint on deviation from the path
+        energy += 0.5*self.fc*sqdists[1]
+        if gpos is not None:
+            gpos[:] += np.einsum('aij,a->ij', self.gpos_cvs, self.fc*self.weights**2*deltas[1])
+        if vtens is not None:
+            vtens[:] += np.einsum('aij,a->ij', self.vtens_cvs, self.fc*self.weights**2*deltas[1])
+        return energy
---
+++ yaff/pes/test/test_bias.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+# YAFF is yet another force-field code.
+# Copyright (C) 2011 Toon Verstraelen <Toon.Verstraelen@UGent.be>,
+# Louis Vanduyfhuys <Louis.Vanduyfhuys@UGent.be>, Center for Molecular Modeling
+# (CMM), Ghent University, Ghent, Belgium; all rights reserved unless otherwise
+# stated.
+#
+# This file is part of YAFF.
+#
+# YAFF is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# YAFF is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>
+#
+# --
+
+
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import pkg_resources
+
+from yaff import *
+
+from yaff.pes.test.common import check_gpos_part, check_vtens_part
+from yaff.test.common import get_system_quartz
+
+from molmod import bend_angle
+from molmod import MolecularGraph
+
+
+def test_bias_harmonicvolume_quartz():
+    system = get_system_quartz()
+    cv = CVVolume(system)
+    K, q0 = 0.4, 0.9*system.cell.volume
+    bias = HarmonicBias(K, q0, cv)
+    part = ForcePartBias(system)
+    part.add_term(bias)
+    e = part.compute()
+    echeck = 0.5*K*(system.cell.volume-q0)**2
+    assert np.abs(e-echeck)<1e-8
+    check_gpos_part(system, part)
+    check_vtens_part(system, part)
+
+def test_bias_multiple_terms():
+    system = get_system_quartz()
+    part = ForcePartBias(system)
+    # Harmonic volume bias
+    cv0 = CVVolume(system)
+    K, q0 = 0.4, 0.9*system.cell.volume
+    bias0 = HarmonicBias(K, q0, cv0)
+    part.add_term(bias0)
+    # Cosine of bending angle
+    cv1 = BendAngle(0,1,2)
+    m, a, phi0 = 1, 2.0, np.pi/4.0
+    bias1 = Cosine(m, a, phi0, cv1)
+    part.add_term(bias1)
+    # Check energy
+    e = part.compute()
+    contributions = part.get_term_energies()
+    assert np.abs(e-np.sum(contributions))<1e-5
+    assert contributions[0]==bias0.compute()
+    # Check derivatives
+    check_gpos_part(system, part)
+    check_vtens_part(system, part)
+    # Check collective variable values
+    cv_values0 = part.get_term_cv_values(0)
+    assert cv_values0.shape[0]==1
+    assert cv_values0[0]==cv0.compute()
+    cv_values1 = part.get_term_cv_values(1)
+    assert cv_values1.shape[0]==1
+    delta0 = system.pos[1] - system.pos[0]
+    system.cell.mic(delta0)
+    delta2 = system.pos[1] - system.pos[2]
+    system.cell.mic(delta2)
+    phi = bend_angle([delta0, np.zeros(3, float), delta2])[0]
+    assert np.abs(cv_values1[0]-phi)<1e-5
+
+def test_bias_distance2plane_dummysystem():
+    # Test a construction for determining the distance between a point and a
+    # plane; the point is defined as an average of two points, the plane is
+    # defined by three points, some again defined as the average of more than
+    # one point.
+    numbers = np.ones((6,), dtype=int)
+    pos = np.zeros((numbers.shape[0],3))
+    system = System(numbers, pos)
+    # Plane defined from points 0,1 and average of 2 and 3:
+    # We choose z=1, so the distance is simply the z coordinate of the point
+    pos[0] = [0.0,0.0,1.0]
+    pos[1] = [1.0,0.0,1.0]
+    pos[2] = [0.0,1.0,1.0]
+    pos[3] = [0.0,1.0,1.0]
+    # Construct COMList; each group is a collection of atoms from which a
+    # position is calculated as a weighted average of atomic positions
+    groups = [ (np.array([0]), np.array([1.0])),
+               (np.array([1]), np.array([1.0])),
+               (np.array([2,3]), np.array([0.5,0.5])),
+               (np.array([4,5]), np.array([0.5,0.5])) ]
+    comlist = COMList(system, groups)
+    bias = ForcePartBias(system, comlist=comlist)
+    # Define a bias potential, harmonic in the distance from group 3 to the
+    # plane spanned by groups 0, 1 and 2
+    ic = OopDist(0,1,2,3)
+    term = Harmonic(0.2,0.1,ic)
+    bias.add_term(term, use_comlist=True)
+    # Define different positions of group 3
+    pointpos = [
+        ([0.0,0.0,2.0],[0.0,0.0,0.0], 0.0),# Point in plane
+        ([6.0,0.0,2.0],[0.0,3.0,2.0], 1.0),# Point above plane
+        ([2.0,-3.,4.0],[0.0,3.0,-8.0], -3.0),# Point below plane
+    ]
+    for r0,r1,distance in pointpos:
+        pos[4] = r0
+        pos[5] = r1
+        bias.compute()
+        d = bias.get_term_cv_values(0)
+        assert d[0]==distance
+
+def test_bias_pathdeviation_mof5():
+    # Load the system
+    fn_system = pkg_resources.resource_filename(__name__, '../../data/test/system_mof5.chk')
+    system = System.from_file(fn_system)
+    # Groups that define the COM
+    atypes = set(['C_B', 'C_B_BR_O', 'O_p', 'B_p','C_HTTP', 'C_O_BR_O', 'C_O'])
+    graph = MolecularGraph(system.bonds, system.numbers)
+    indices = graph.independent_vertices
+    groups = []
+    for layer in [0,1]:
+        groups.append( [iatom for iatom in indices[layer] if system.get_ffatype(iatom) in atypes] )
+    # Collective Variables
+    cv0 = CVCOMProjection(system, groups, 0)
+    cv1 = CVCOMProjection(system, groups, 1)
+    # The path, rather random nonsense
+    npoints = 100
+    path = np.zeros((npoints,3))
+    path[:,0] = np.cos(np.linspace(0,np.pi,npoints))*0.1
+    path[:,1] = np.sin(np.linspace(0,np.pi,npoints))*-0.1
+    path[:,2] = 0.2*path[:,0]+path[:,1]**2
+    # Test without the harmonic restraint
+    bias = PathDeviationBias([cv0,cv1], path, 0.0)
+    part = ForcePartBias(system)
+    part.add_term(bias)
+    check_gpos_part(system, part)
+    check_vtens_part(system, part)
+    # Test without the harmonic restraint, closest point on path is starting
+    # point of the path
+    bias = PathDeviationBias([cv0,cv1], path[27:], 0.0)
+    index, _, _ = bias.find_nearest_point(np.array([cv.compute() for cv in bias.cvs]))
+    assert index==0
+    part = ForcePartBias(system)
+    part.add_term(bias)
+    check_gpos_part(system, part)
+    check_vtens_part(system, part)
+    # Test with the harmonic restraint
+    bias = PathDeviationBias([cv0,cv1], path, 0.5)
+    part = ForcePartBias(system)
+    part.add_term(bias)
+    check_gpos_part(system, part)
+    check_vtens_part(system, part)
