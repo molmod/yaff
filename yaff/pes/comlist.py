Fixed the computation of the vtens due to valence forces computed through a COMList

yaff/pes/comlist.py
@@ -93,10 +93,13 @@ class COMList(object):
         dlist_forward(self.system.pos, self.system.cell, self.deltas, len(self.deltas))
         comlist_forward(self.deltas, self.system.pos, self.pos, self.comsizes, self.comtab)
 
-    def back(self, gpos, vtens):
-        """Derive gpos and virial from the derivatives towards the relative vectors
+    def back(self, gpos):
+        """Derive gpos from the derivatives towards the relative vectors
+           dlist_back() is executed with a dummy vtens, because the vtens from
+           comlist interactions is computed elsewhere.
+           (See the _internal_compute() method of ``ForcePartValence``)
 
            The actual computation is carried out by a low-level C routine.
         """
         comlist_back(self.deltas, gpos, self.gpos, self.comsizes, self.comtab)
-        dlist_back(gpos, vtens, self.deltas, len(self.deltas))
+        dlist_back(gpos, np.zeros((3, 3)), self.deltas, len(self.deltas))